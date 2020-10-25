9372b192c32a62446d189fc27d6a5df194904d9e
IanCaminhas <iancaminhas25@hotmail.com>
2020-10-25 18:37:45 -0300

    Basico sobre funcao 01

--- funcoes/funcao_1.js
+++ funcoes/funcao_1.js
@@ -1,1 +1,35 @@
-console.log('Teste')
+let a = 4
+console.log(a)
+
+
+function bomDia(){
+    console.log('bom dia')
+}
+
+bomDia()
+
+const boaTarde = function (){
+    console.log('Boa tarde')
+}
+
+boaTarde()
+
+function somar(a=0,b=0){
+    return a+ b
+}
+
+let resultado= somar(3,4)
+console.log(resultado)
+
+resultado= somar(3,4,5,6,7,8)
+console.log(resultado)
+
+resultado= somar(3)
+console.log(resultado)
+
+resultado= somar()
+console.log(resultado)
+
+
+
+
